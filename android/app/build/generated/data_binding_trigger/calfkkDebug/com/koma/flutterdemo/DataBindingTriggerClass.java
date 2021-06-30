f1efac35d4406b3c316bf72f99e80426927431b0
chenhang <chenhang@feng-go.com>
2021-06-30 12:00:47 +0800

    修改ignore文件

--- android/app/build/generated/data_binding_trigger/calfkkDebug/com/koma/flutterdemo/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.koma.flutterdemo;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- android/app/build/generated/source/kapt/calfkkDebug/com/koma/flutterdemo/BR.java
+++
@@ -1,5 +0,0 @@
-package com.koma.flutterdemo;
-
-public class BR {
-  public static final int _all = 0;
-}
