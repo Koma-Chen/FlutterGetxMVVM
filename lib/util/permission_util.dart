8f30633f5edd17334a199f2597102cf6ed631c52
Koma <shishangmeihuo@gmail.com>
2021-08-30 19:39:42 +0800

    添加权限申请

---
+++ lib/util/permission_util.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:permission/permission.dart';
+
+class PermissionUtil {
+  static Future<bool> requestPermissions(
+      List<PermissionName> permissionNameList) async {
+    // [PermissionName.Storage, PermissionName.Microphone]
+
+    if (Platform.isIOS) return true;
+
+    final List<Permissions> list =
+        await Permission.requestPermissions(permissionNameList);
+    bool allow = true;
+    list.forEach((permission) {
+      if (permission.permissionStatus != PermissionStatus.allow) {
+        allow = false;
+      }
+    });
+    return allow;
+  }
+
+  static Future<bool> checkStoragePermission() async {
+    return requestPermissions([PermissionName.Storage]);
+  }
+}
